ed4a3167a1a08bc5596aef3bcd35958d2ced0b89
Augustas Igneris <augustigner@gmail.com>
2024-02-04 23:45:06 +0000

    Day 4

---
+++ Solutions/Day4/Solution.cs
@@ -0,0 +1,49 @@
+﻿string[] input = File.ReadAllLines("Input.txt");
+
+PartOne(input); // Answer 580
+PartTwo(input); // Answer 895
+
+static void PartOne(string[] input)
+{
+    int overlapping = 0;
+    foreach (string line in input)
+    {
+        var pairs = line.Split(',');
+        var pair1Sections = pairs[0].Split("-").Select(s => int.Parse(s)).ToArray();
+        var pair2Sections = pairs[1].Split("-").Select(s => int.Parse(s)).ToArray();
+
+        if ((pair1Sections[0] <= pair2Sections[0] && pair1Sections[1] >= pair2Sections[1]) ||
+            (pair2Sections[0] <= pair1Sections[0] && pair2Sections[1] >= pair1Sections[1]))
+        {
+            overlapping++;
+        }
+
+    }
+
+    Console.WriteLine($"Total number of pairs with completely overlapping sections are: {overlapping}");
+}
+
+static void PartTwo(string[] input)
+{
+    int overlapping = 0;
+    foreach (string line in input)
+    {
+        var pairs = line.Split(',');
+        var pair1Sections = pairs[0].Split("-").Select(s => int.Parse(s)).ToArray();
+        var pair2Sections = pairs[1].Split("-").Select(s => int.Parse(s)).ToArray();
+
+        if (IsSectionInRange(pair1Sections, pair2Sections[0]) || IsSectionInRange(pair1Sections, pair2Sections[1]) ||
+            IsSectionInRange(pair2Sections, pair1Sections[0]) || IsSectionInRange(pair2Sections, pair1Sections[1]))
+        {
+            overlapping++;
+        }
+
+    }
+
+    Console.WriteLine($"Total number of pairs with any overlapping sections are: {overlapping}");
+}
+
+static bool IsSectionInRange(int[] sectionRange, int section)
+{
+    return section >= sectionRange[0] && section <= sectionRange[1];
+}
