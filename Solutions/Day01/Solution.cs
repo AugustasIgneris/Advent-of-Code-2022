0ee332eeb944b433135c457f97272c4edf8037c2
Augustas Igneris <augustigner@gmail.com>
2024-02-12 22:16:02 +0000

    Fix project alphabetical ordering

---
+++ Solutions/Day01/Solution.cs
@@ -0,0 +1,60 @@
+﻿string[] input = File.ReadAllLines("Input.txt");
+
+PartOne(input); // Answer 70296
+PartTwo(input, 3); // Answer 205381
+
+static void PartOne(string[] input) {
+    int currentCallories = 0;
+    int highestCalorries = 0;
+
+    foreach (string line in input)
+    {
+        if (line == "")
+        {
+            if (highestCalorries < currentCallories)
+            {
+                highestCalorries = currentCallories;
+            }
+
+            currentCallories = 0;
+        }
+        else
+        {
+            currentCallories += int.Parse(line);
+        }
+    }
+
+    Console.WriteLine($"The highest number of callories carried by an elf is: {highestCalorries}");
+}
+
+static void PartTwo(string[] input, int numberOfTopElves)
+{
+    int currentCallories = 0;
+    var topCalorries = new PriorityQueue<int, int>(3);
+
+    foreach (string line in input)
+    {
+        if (line == "")
+        {
+            if (topCalorries.Count < numberOfTopElves || topCalorries.Peek() < currentCallories)
+            {
+                topCalorries.Enqueue(currentCallories, currentCallories);
+            }
+
+            if (topCalorries.Count > numberOfTopElves)
+            {
+                topCalorries.Dequeue();
+            }
+
+            currentCallories = 0;
+        }
+        else
+        {
+            currentCallories += int.Parse(line);
+        }
+    }
+
+    var totalTopCallories = topCalorries.UnorderedItems.Sum(e => e.Element);
+
+    Console.WriteLine($"The total number of callories carried by the top {numberOfTopElves} is: {totalTopCallories}");
+}
---
+++ Solutions/Day07/Solution.cs
@@ -0,0 +1,107 @@
+﻿string[] input = File.ReadAllLines("Input.txt");
+List<Directory> sizes = GetDirectorySizes(input);
+
+PartOne(sizes); // Answer 1444896
+PartTwo(sizes); // Answer 404395
+
+static void PartOne(List<Directory> directories)
+{
+    int totalSize = directories.Where(d => d.Size < 100000).Sum(d => d.Size);
+    Console.WriteLine($"The total size of directories under size 100000 is: {totalSize}");
+}
+
+static void PartTwo(List<Directory> directories)
+{
+    Directory closest = directories[0];
+
+    int spaceNeeded = 30000000 - (70000000 - directories[0].Size);
+
+    foreach (var dir in directories)
+    {
+        if (dir.Size >= spaceNeeded && dir.Size < closest.Size)
+        {
+            closest = dir;
+        }
+    }
+
+    Console.WriteLine($"The size of the smallest directory larger than {spaceNeeded} is {closest.Size}");
+}
+
+
+static List<Directory> GetDirectorySizes(string[] input)
+{
+    var directories = new List<Directory>();
+    var dirStack = new Stack<Directory>();
+    var cd = "cd".AsSpan();
+    var root = "/".AsSpan();
+    var popd = "..".AsSpan();
+    var dir = "dir".AsSpan();
+    var commandPrefix = "$".AsSpan();
+
+    void PopDirectory()
+    {
+        Directory complete = dirStack.Pop();
+        dirStack.Peek().Size += complete.Size;
+    }
+
+    foreach (string line in input)
+    {
+        var lineSpan = line.AsSpan();
+
+        if (lineSpan.StartsWith(commandPrefix))
+        {
+            // Only cd and ls are supported
+            // ls requires no action
+            var command = lineSpan.Slice(2, 2);
+            if (command.SequenceEqual(cd))
+            {
+                var arg = lineSpan.Slice(5);
+                if (arg.SequenceEqual(root) && dirStack.Count > 0)
+                {
+                    while (dirStack.Count > 1)
+                    {
+                        PopDirectory();
+                    }
+                }
+                else if (arg.SequenceEqual(popd))
+                {
+                    PopDirectory();
+                }
+                else
+                {
+                    var directory = new Directory(arg.ToString());
+                    dirStack.Push(directory);
+                    directories.Add(directory);
+                }
+            }
+        }
+        else if (lineSpan.StartsWith(dir))
+        {
+            continue;
+        }
+        else
+        {
+            int size = int.Parse(lineSpan.Slice(0, lineSpan.IndexOf(' ') + 1));
+            dirStack.Peek().Size += size;
+        }
+    }
+
+    while (dirStack.Count > 1)
+    {
+        PopDirectory();
+    }
+
+    return directories;
+}
+
+
+record Directory
+{
+    public Directory(string Name)
+    {
+        this.Name = Name;
+    }
+
+    public int Size { get; set; } = 0;
+    public string Name { get; }
+}
