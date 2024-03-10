34e9fb7a72bf6be4d079752cbbcce1fe114136b6
Augustas Igneris <augustas.igneris@microfocus.com>
2024-03-10 18:52:50 +0000

    Days 13 and 14

---
+++ Solutions/Day13/Solution.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections.ObjectModel;
+
+string[] input = File.ReadAllLines("Input.txt");
+
+PartOne(input); // 5198
+PartTwo(input); // 22344
+
+static void PartOne(string[] input)
+{
+    int sum = 0;
+
+    foreach ((int index, PacketContent left, PacketContent right) in GetPairs(input))
+    {
+        if (left.CompareTo(right) < 0)
+        {
+            sum += index;
+        }
+    }
+
+    Console.WriteLine($"The sum of indeces of correctly ordered packets is {sum}");
+}
+
+static void PartTwo(string[] input)
+{
+    List<PacketContent> packets = [];
+
+    var divider1 = PacketContent.Parse("[[2]]");
+    var divider2 = PacketContent.Parse("[[6]]");
+
+    packets.Add(divider1);
+    packets.Add(divider2);
+
+    foreach (string line in input)
+    {
+        if (line == "")
+        {
+            continue;
+        }
+
+        packets.Add(PacketContent.Parse(line));
+    }
+
+    packets.Sort();
+
+    int key = (packets.IndexOf(divider1) + 1) * (packets.IndexOf(divider2) + 1);
+    Console.WriteLine($"The decoder key is {key}");
+}
+
+static IEnumerable<(int index, PacketContent left, PacketContent right)> GetPairs(string[] input) {
+    int pair = 1;
+    for (int i = 0; i < input.Length; i+=3)
+    {
+        yield return (pair++, PacketContent.Parse(input[i]), PacketContent.Parse(input[i + 1]));
+    }
+}
+
+record PacketContent : IComparable<PacketContent>
+{
+    public IReadOnlyList<PacketContent> Children { get; }
+    public int? Value { get; }
+
+    PacketContent(IReadOnlyList<PacketContent> children)
+    {
+        Children = children;
+    }
+
+    PacketContent(int value)
+    {
+        Children = ReadOnlyCollection<PacketContent>.Empty;
+        Value = value;
+    }
+
+    public int CompareTo(PacketContent? other)
+    {
+        return CompareTo(other, 0);
+    }
+
+
+    public int CompareTo(PacketContent? other, int level)
+    {
+        level++;
+
+        if (other == null)
+        {
+            return -1;
+        }
+
+        if (Value.HasValue)
+        {
+            if (other.Value.HasValue)
+            {
+                // Compare two values directly
+                return Value.Value.CompareTo(other.Value);
+            }
+            else
+            {
+                // Wrap this value so it can be compared
+                return new PacketContent([this]).CompareTo(other, level);
+            }
+        }
+        else if (other.Value.HasValue)
+        {
+            // Wrap other value so it can be compared
+            return CompareTo(new PacketContent([other]), level);
+        }
+
+        // Compare two lists
+        for (int i = 0; i < Children.Count; i++)
+        {
+            if (!(other.Children.Count > i))
+            {
+                // Other list is shorter thus this list is greater
+                return 1;
+            }
+
+            int ret = Children[i].CompareTo(other.Children[i], level);
+            if (ret != 0)
+            {
+                return ret;
+            }
+        }
+
+        if (Children.Count < other.Children.Count)
+        {
+            // Other list is longer thus this list is lesser
+            return -1;
+        }
+
+        return 0;
+    }
+
+    public static PacketContent Parse(string packetSpan)
+    {
+        Stack<List<PacketContent>> listStack = new();
+
+        for (int i = 0; i < packetSpan.Length; i++)
+        {
+            char currentChar = packetSpan[i];
+            if (currentChar == '[')
+            {
+                listStack.Push([]);
+            }
+            else if (currentChar == ']')
+            {
+                var child = new PacketContent(listStack.Pop());
+
+                if (listStack.TryPeek(out List<PacketContent>? current))
+                {
+                    current.Add(child);
+                }
+                else
+                {
+                    return child;
+                }
+            }
+            else if (currentChar == ',')
+            {
+                continue;
+            }
+            else
+            {
+                int endIndex = packetSpan.IndexOfAny(['[', ']', ','], i + 1) - 1;
+                int number;
+
+                if (endIndex == i)
+                {
+                    number = (int)char.GetNumericValue(currentChar);
+                }
+                else
+                {
+                    number = int.Parse(packetSpan.AsSpan(i, endIndex - i + 1));
+                    i = endIndex;
+                }
+
+                listStack.Peek().Add(new PacketContent(number));
+            }
+        }
+
+        throw new InvalidDataException(packetSpan);
+    }
+
+    public override string? ToString()
+    {
+        if (Value.HasValue)
+        {
+            return Value.Value.ToString();
+        }
+        else
+        {
+            return '[' + string.Join(',', Children) + ']';
+        }
+    }
+}
---
+++ Solutions/Day14/Solution.cs
@@ -0,0 +1,290 @@
+﻿string[] input = File.ReadAllLines("Input.txt");
+
+PartOne(input); // 1298
+PartTwo(input); // 25585
+
+static void PartOne(string[] input)
+{
+    char[,] cave = ReadCave(input, true, out Node hole);
+
+    //Console.WriteLine("========================== BEFORE ==========================\n");
+    //PrintCave(cave);
+    int sandAtRest = 0;
+    bool fellThrough = false;
+    while (!fellThrough)
+    {
+        int sandX = hole.x;
+        int sandY = hole.y;
+
+        while (true)
+        {
+            int newSandY = sandY + 1;
+
+            if (newSandY > cave.GetLength(1))
+            {
+                // Sand fell down below any of the rocks
+                fellThrough = true;
+                break;
+            }
+
+            if (cave[sandX, newSandY] == '.')
+            {
+                // Fell straight down
+                sandY = newSandY;
+                continue;
+            }
+
+            // Check if sand can fall to the left
+            int newSandX = sandX - 1;
+
+            if (newSandX < 0)
+            {
+                // Sand falls to the void on the left
+                fellThrough = true;
+                break;
+            }
+
+
+            if (cave[newSandX, newSandY] == '.')
+            {
+                sandY = newSandY;
+                sandX = newSandX;
+                continue;
+            }
+
+            // Check if sand can fall to the right
+            newSandX = sandX + 1;
+
+            if (newSandX >= cave.GetLength(0))
+            {
+                // Sand falls to the void on the right
+                fellThrough = true;
+                break;
+            }
+
+            if (cave[newSandX, newSandY] == '.')
+            {
+                sandY = newSandY;
+                sandX = newSandX;
+                continue;
+            }
+
+            // Nowhere to go - sand is at rest
+            cave[sandX, sandY] = 'o';
+            sandAtRest += 1;
+            break;
+        }
+    }
+
+    //Console.WriteLine("\n========================== AFTER ==========================\n");
+    //PrintCave(cave);
+    Console.WriteLine($"PART ONE: The units of sand that came to rest is {sandAtRest}");
+}
+
+static void PartTwo(string[] input)
+{
+    char[,] cave = ReadCave(input, false, out Node hole);
+
+    //Console.WriteLine("========================== BEFORE ==========================\n");
+    //PrintCave(cave);
+    int sandAtRest = 0;
+    bool blockedSource = false;
+    while (!blockedSource)
+    {
+        int sandX = hole.x;
+        int sandY = hole.y;
+
+        while (true)
+        {
+            int newSandY = sandY + 1;
+
+            if (cave[sandX, newSandY] == '.')
+            {
+                // Fell straight down
+                sandY = newSandY;
+                continue;
+            }
+
+            // Check if sand can fall to the left
+            int newSandX = sandX - 1;
+
+            if (cave[newSandX, newSandY] == '.')
+            {
+                sandY = newSandY;
+                sandX = newSandX;
+                continue;
+            }
+
+            // Check if sand can fall to the right
+            newSandX = sandX + 1;
+
+            if (cave[newSandX, newSandY] == '.')
+            {
+                sandY = newSandY;
+                sandX = newSandX;
+                continue;
+            }
+
+            // Nowhere to go - sand is at rest
+            cave[sandX, sandY] = 'o';
+            sandAtRest += 1;
+
+            if (sandX == hole.x && sandY == hole.y)
+            {
+                blockedSource = true;
+            }
+
+            break;
+        }
+    }
+
+    //Console.WriteLine("\n========================== AFTER ==========================\n");
+    //PrintCave(cave);
+    Console.WriteLine($"PART TWO: The units of sand that came to rest is {sandAtRest}");
+}
+
+static char[,] ReadCave(string[] input, bool finite, out Node hole)
+{
+    char[,] cave = new char[750, 750];
+    int minX = int.MaxValue;
+    int maxX = 0;
+    int minY = 0;
+    int maxY = 0;
+    cave[500, 0] = '*';
+
+    foreach (string line in input)
+    {
+        Node? previous = null;
+
+        foreach (Node node in GetNodes(line))
+        {
+            if (previous != null)
+            {
+                int xDiff = node.x - previous.Value.x;
+                if (xDiff != 0)
+                {
+                    int increment = Math.Sign(xDiff);
+                    for (int x = previous.Value.x; x != node.x;)
+                    {
+                        x += increment;
+                        cave[x, node.y] = '#';
+                    }
+                }
+                else
+                {
+                    int yDiff = node.y - previous.Value.y;
+                    int increment = Math.Sign(yDiff);
+
+                    for (int y = previous.Value.y; y != node.y;)
+                    {
+                        y += increment;
+                        cave[node.x, y] = '#';
+                    }
+                }
+            }
+            else
+            {
+                cave[node.x, node.y] = '#';
+            }
+
+            previous = node;
+        }
+    }
+
+    for (int y = 0; y < cave.GetLength(1); y++)
+    {
+        for (int x = 0; x < cave.GetLength(0); x++)
+        {
+            if (cave[x, y] == '\0')
+            {
+                // Fill the cave with 'air' to match the puzzle better
+                cave[x, y] = '.';
+            }
+            else
+            {
+                if (x > maxX)
+                {
+                    maxX = x;
+                }
+                else if (x < minX)
+                {
+                    minX = x;
+                }
+                if (y > maxY)
+                {
+                    maxY = y;
+                }
+            }
+        }
+    }
+
+    if (!finite)
+    {
+        minX -= 50;
+        maxX += 150;
+        maxY += 2;
+    }
+
+    char[,] trimmedCave = new char[maxX - minX + 1, maxY - minY + 1];
+
+    for (int x = minX; x <= maxX; x++)
+    {
+        for (int y = minY; y <= maxY; y++)
+        {
+            trimmedCave[x - minX, y - minY] = cave[x, y];
+        }
+    }
+
+    hole = new Node(500 - minX, 0);
+    cave = trimmedCave;
+
+    if (!finite)
+    {
+        for (int x = 0; x < cave.GetLength(0); x++)
+        {
+            cave[x, maxY] = '#';
+        }
+    }
+
+    return cave;
+}
+
+static IEnumerable<Node> GetNodes(string line)
+{
+    int start = 0;
+    while (start < line.Length)
+    {
+        int xEnd = line.IndexOf(',', start);
+        int yEnd = line.IndexOf(' ', xEnd);
+
+        if (yEnd == -1)
+        {
+            yEnd = line.Length - 1;
+        }
+
+        int x = int.Parse(line.AsSpan(start, xEnd - start));
+        int y = int.Parse(line.AsSpan(xEnd + 1, yEnd - xEnd));
+
+        yield return new Node(x, y);
+
+        start = yEnd + 4;
+    }
+}
+
+static void PrintCave(char[,] cave)
+{
+    for (int y = 0; y < cave.GetLength(1); y++)
+    {
+        for (int x = 0; x < cave.GetLength(0); x++)
+        {
+            Console.Write(cave[x, y]);
+        }
+        Console.WriteLine();
+    }
+}
+
+record struct Node(int x, int y)
+{
+    public static implicit operator (int x, int y)(Node value) => (value.x, value.y);
+    public static implicit operator Node((int x, int y) value) => new(value.x, value.y);
+}
